36451ed30c9d94bd05ee690ba0eece163dd56ff3
ubgamesuk <tlowdon10@outlook.com>
2025-06-01 14:34:28 +0100

    New app

---
+++ app.js
@@ -0,0 +1,384 @@
+        function closeGame1() {
+      var x = document.getElementById("game1");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame1() {
+      var x = document.getElementById("game1");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame2() {
+      var x = document.getElementById("game2");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame2() {
+      var x = document.getElementById("game2");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame3() {
+      var x = document.getElementById("game3");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame3() {
+      var x = document.getElementById("game3");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame4() {
+      var x = document.getElementById("game4");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame4() {
+      var x = document.getElementById("game4");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame5() {
+      var x = document.getElementById("game5");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame5() {
+      var x = document.getElementById("game5");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame6() {
+      var x = document.getElementById("game6");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame6() {
+      var x = document.getElementById("game6");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame7() {
+      var x = document.getElementById("game7");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame7() {
+      var x = document.getElementById("game7");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame8() {
+      var x = document.getElementById("game8");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame8() {
+      var x = document.getElementById("game8");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame9() {
+      var x = document.getElementById("game9");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame9() {
+      var x = document.getElementById("game9");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame10() {
+      var x = document.getElementById("game10");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame10() {
+      var x = document.getElementById("game10");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame11() {
+      var x = document.getElementById("game11");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame11() {
+      var x = document.getElementById("game11");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame12() {
+      var x = document.getElementById("game12");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame12() {
+      var x = document.getElementById("game12");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame13() {
+      var x = document.getElementById("game13");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame13() {
+      var x = document.getElementById("game13");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame14() {
+      var x = document.getElementById("game14");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame14() {
+      var x = document.getElementById("game14");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame15() {
+      var x = document.getElementById("game15");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame15() {
+      var x = document.getElementById("game15");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame16() {
+      var x = document.getElementById("game16");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame16() {
+      var x = document.getElementById("game16");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame17() {
+      var x = document.getElementById("game17");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame17() {
+      var x = document.getElementById("game17");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame18() {
+      var x = document.getElementById("game18");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame19() {
+      var x = document.getElementById("game19");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame20() {
+      var x = document.getElementById("game20");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame20() {
+      var x = document.getElementById("game20");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame21() {
+      var x = document.getElementById("game21");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame21() {
+      var x = document.getElementById("game21");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame22() {
+      var x = document.getElementById("game22");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame22() {
+      var x = document.getElementById("game22");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame23() {
+      var x = document.getElementById("game23");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame23() {
+      var x = document.getElementById("game23");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame24() {
+      var x = document.getElementById("game24");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame24() {
+      var x = document.getElementById("game24");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+            function closeGame25() {
+      var x = document.getElementById("game25");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openGame25() {
+      var x = document.getElementById("game25");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
